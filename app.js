4d2586b1b0039799010af63e8d56793daace47aa
Peter Donald <peter@realityforge.org>
2017-09-17 13:56:12 +1000

    Separate logic and effects

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-xs.periodic(1000).fold(prev => prev + 1, 0).map(i => `Seconds elapsed ${i}`).subscribe({
-  next: message => {
-    const app = document.querySelector('#app');
-    app.textContent = message;
-  }
-});
+function main() {
+  return xs.periodic(1000).fold(prev => prev + 1, 0).map(i => `Seconds elapsed ${i}`);
+}
+
+function domDriver(text$) {
+  text$.subscribe({
+    next: message => {
+      const app = document.querySelector('#app');
+      app.textContent = message;
+    }
+  });
+}
+
+const sink = main();
+domDriver(sink);
