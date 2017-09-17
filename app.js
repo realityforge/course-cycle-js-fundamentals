d64691c3e1aa779558a696e109be062ccfe2c5b8
Peter Donald <peter@realityforge.org>
2017-09-18 09:20:11 +1000

    Re-add source using cycle dom driver

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const { h1, makeDOMDriver } = CycleDOM;
 
 function main(sources) {
-  const click$ = sources.DOM;
+  const mouseover$ = sources.DOM.select('h1').events('mouseover');
   return {
-    DOM: xs.periodic(1000).fold(prev => prev + 1, 0).map(i => h1(`Seconds elapsed ${i}`)),
-    // This data should go to Log effect
+    DOM: mouseover$.startWith(null)
+                   .map(() => xs.periodic(1000).fold(prev => prev + 1, 0))
+                   .flatten().map(i => h1(`Seconds elapsed ${i}`)),
     Log: xs.periodic(2000).fold(prev => prev + 1, 0)
   };
 }
