af2240b203514951bb70bee7d4b2c29e81e33e22
Peter Donald <peter@realityforge.org>
2017-09-18 08:50:31 +1000

    Hand-write the generic version

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-function main() {
+function main(sources) {
+  const click$ = sources.DOM;
   return {
     // This data goes to DOM effect
-    DOM: xs.periodic(1000).fold(prev => prev + 1, 0).map(i => `Seconds elapsed ${i}`),
+    DOM: click$.startWith(null)
+               .map(() => xs.periodic(1000).fold(prev => prev + 1, 0))
+               .flatten().map(i => `Seconds elapsed ${i}`),
     // This data should go to Log effect
     Log: xs.periodic(2000).fold(prev => prev + 1, 0)
   };
@@ -14,6 +17,8 @@ function domDriver(text$) {
       app.textContent = message;
     }
   });
+  const domSource = fromEvent(document, 'click');
+  return domSource;
 }
 
 function logDriver(msg$) {
@@ -25,11 +30,25 @@ function logDriver(msg$) {
 }
 
 function run(mainFn, drivers) {
-  const sinks = mainFn();
+  const fakeSinks = {};
   Object.keys(drivers).forEach(key => {
-      if (sinks[key]) {
-        drivers[key](sinks[key]);
-      }
+    // This is a fake that we can later make to imitate the real sink
+    // This is used as the driver has a cyclic dependency (and this is
+    // how Cycle.js gets it's name).
+    fakeSinks[key] = xs.create();
+  });
+
+  const sources = {};
+  Object.keys(drivers).forEach(key => {
+      // sources[key] ends up being the real source for every driver
+      sources[key] = drivers[key](fakeSinks[key]);
+    }
+  );
+
+  const sinks = mainFn(sources);
+
+  Object.keys(drivers).forEach(key => {
+      fakeSinks[key].imitate(sinks[key]);
     }
   );
 }
