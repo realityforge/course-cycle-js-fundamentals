73cb8470d9042b0437ad340a006128ec8539e1ec
Peter Donald <peter@realityforge.org>
2017-09-17 13:54:07 +1000

    Format

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
- xs.periodic(1000).//  ---!---!---!---!
-  fold(prev => prev + 1, 0).// 0---1---2---3---4
-    map(i => `Seconds elapsed ${i}`).subscribe({
-    next: message => {
-      const app = document.querySelector('#app');
-      app.textContent = message;
-    }
-  });
+xs.periodic(1000).fold(prev => prev + 1, 0).map(i => `Seconds elapsed ${i}`).subscribe({
+  next: message => {
+    const app = document.querySelector('#app');
+    app.textContent = message;
+  }
+});
