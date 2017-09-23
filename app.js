95af26f11470f00d5882e10bf44cf1c380ea98dc
Peter Donald <peter@realityforge.org>
2017-09-23 15:06:10 +1000

    Create a Generic Cycle.js Component

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
 const { h1, h4, a, button, div, label, input, makeDOMDriver } = CycleDOM;
-const { makeHTTPDriver } = CycleHTTPDriver;
 
 // This is all the input effects or intents (it represents user intentions)
 function intent(domSource) {
-  const changeWeight$ = domSource.select('.weight').events('input').map(e => e.target.value);
-  const changeHeight$ = domSource.select('.height').events('input').map(e => e.target.value);
-  return { changeWeight$, changeHeight$ };
+  const changeValue$ = domSource.select('.slider').events('input').map(e => e.target.value);
+  return { changeValue$ };
 }
 
 // This is the application model
-function model(actions) {
-  const { changeWeight$, changeHeight$ } = actions;
-
-  const bmi$ = xs.combine(changeWeight$.startWith(70), changeHeight$.startWith(160)).map(([weight, height]) => {
-    const heightInMeters = height * 0.01;
-    // BMI calculation
-    const bmi = Math.round(weight / (heightInMeters * heightInMeters));
-    return { weight: weight, height: height, bmi: bmi };
-  });
-  return bmi$;
+function model(actions, props$) {
+  const { changeValue$ } = actions;
+  return props$.map(props => {
+    return changeValue$.startWith(props.initial).map(value => {
+      return {
+        value,
+        min: props.min,
+        max: props.max,
+        label: props.label,
+        units: props.units
+      };
+    });
+  }).flatten();
 }
 
 //This is the output effects aka the view
 function view(state$) {
-  const virtualDom$ = state$.map(state =>
-    div([
-      div([
-        label('Weight: ' + state.weight + 'kg'),
-        input('.weight', { attrs: { type: 'range', min: 40, max: 160, value: state.weight } })
-      ]),
-      div([
-        label('Height: ' + state.height + 'cm'),
-        input('.height', { attrs: { type: 'range', min: 150, max: 220, value: state.height } })
-      ]),
-      h4('BMI is ' + state.bmi)
-    ])
+  return state$.map(state =>
+    div('.labeled-slider', [
+        label('.label', state.label + ': ' + state.value + state.units),
+        input('.slider', { attrs: { type: 'range', min: state.min, max: state.max, value: state.weight } })
+      ]
+    )
   );
-  return virtualDom$;
 }
 
 function main(sources) {
-  /*
-   * This architecture is known as model view intent
-   */
+
+  const props$ = sources.props;
 
   const actions = intent(sources.DOM);
-  const state$ = model(actions);
+  const state$ = model(actions, props$);
   const virtualDom$ = view(state$);
   return {
     DOM: virtualDom$
   };
 }
 
-Cycle.run(main, { DOM: makeDOMDriver('#app'), HTTP: makeHTTPDriver() });
+Cycle.run(main,
+  {
+    DOM: makeDOMDriver('#app'),
+    props: () => xs.of({
+      label: 'Weight',
+      units: 'kg',
+      min: 40,
+      max: 150,
+      initial: 40
+    })
+  });
