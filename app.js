6041ef4a73df12886cfddfde4d39129bdfb0df34
Peter Donald <peter@realityforge.org>
2017-09-23 14:50:08 +1000

    COnvert to model view intent

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const { h1, h4, a, button, div, label, input, makeDOMDriver } = CycleDOM;
 const { makeHTTPDriver } = CycleHTTPDriver;
 
-function main(sources) {
-  const changeWeight$ = sources.DOM.select('.weight').events('input').map(e => e.target.value);
-  const changeHeight$ = sources.DOM.select('.height').events('input').map(e => e.target.value);
+// This is all the input effects or intents (it represents user intentions)
+function intent(domSource) {
+  const changeWeight$ = domSource.select('.weight').events('input').map(e => e.target.value);
+  const changeHeight$ = domSource.select('.height').events('input').map(e => e.target.value);
+  return { changeWeight$, changeHeight$ };
+}
+
+// This is the application model
+function model(actions) {
+  const { changeWeight$, changeHeight$ } = actions;
 
   const bmi$ = xs.combine(changeWeight$.startWith(70), changeHeight$.startWith(160)).map(([weight, height]) => {
     const heightInMeters = height * 0.01;
@@ -11,9 +18,12 @@ function main(sources) {
     const bmi = Math.round(weight / (heightInMeters * heightInMeters));
     return { weight: weight, height: height, bmi: bmi };
   });
+  return bmi$;
+}
 
-  // display data
-  const virtualDom$ = bmi$.map(state =>
+//This is the output effects aka the view
+function view(state$) {
+  const virtualDom$ = state$.map(state =>
     div([
       div([
         label('Weight: ' + state.weight + 'kg'),
@@ -26,6 +36,17 @@ function main(sources) {
       h4('BMI is ' + state.bmi)
     ])
   );
+  return virtualDom$;
+}
+
+function main(sources) {
+  /*
+   * This architecture is known as model view intent
+   */
+
+  const actions = intent(sources.DOM);
+  const state$ = model(actions);
+  const virtualDom$ = view(state$);
   return {
     DOM: virtualDom$
   };
